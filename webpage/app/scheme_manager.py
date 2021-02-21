b31f09cdf1d3a699c64983e01944b1abe9b88d58
NicholasGoh <gohn0004@e.ntu.edu.sg>
2021-02-22 00:41:25 +0800

    Revert "added webpage"
    This reverts commit c73376c08fa9c0d46bc8e9924ab9fbe95e2d7d4e.

--- webpage/app/scheme_manager.py
+++
@@ -1,7 +0,0 @@
-from app.schemes.housing_grants_for_ec import HousingGrantsForEC
-
-class SchemeManager():
-    def __init__(self, inputs):
-        self.scheme1Output = HousingGrantsForEC(inputs).returnBracket()
-    def foo(self):
-        return [self.scheme1Output]
--- webpage/app/schemes/housing_grants_for_ec.py
+++
@@ -1,9 +0,0 @@
-class HousingGrantsForEC():
-    def __init__(self, inputs):
-        self.age = inputs.age
-        pass
-    def returnBracket(self):
-        if 30 < self.age.data < 40:
-            return 1000
-        else:
-            return 2000
