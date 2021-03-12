8da63e3c75d8c95b74d8ba22d101331dbb063b01
jerpong98 <jeremypong12@gmail.com>
2021-03-12 11:25:32 +0800

    no comments hehe

---
+++ google_map_sample_v2/googleMap.js
@@ -0,0 +1,63 @@
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+function initMap() {
+
+  // function to read the json file
+  function readJSON(file, callback){
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', file, true);
+    xobj.onreadystatechange = function(){
+      if (xobj.readyState == 4 && xobj.status =='200'){
+        callback(xobj.responseText);
+      }
+    };
+    xobj.send(null);
+  };
+
+  // call to read JSON and place all the markers
+  readJSON('transactions.json', function(data) {
+    var mydata = JSON.parse(data);
+    const markers = [];
+    const properties = [];
+
+    // extract all the properties from the JSON and push into arrays
+    for (const [propName, value] of Object.entries(mydata)){
+      markers.push(value);
+      properties.push(propName);
+    };
+
+    // initializing a new map
+    var mapOptions = {
+      center: new google.maps.LatLng(markers[0].x, markers[0].y),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+
+    //Create and open InfoWindow.
+    var infoWindow = new google.maps.InfoWindow();
+    for (var i = 0; i < 20; i++) {
+        var data = markers[i];
+        var property = properties[i];
+        var myLatlng = new google.maps.LatLng(data.x, data.y);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: property
+        });
+
+        // Attach click event to the marker.
+        (function (marker, data) {
+            google.maps.event.addListener(marker, "mouseover", function (e) {
+                infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + marker.title + "</div>");
+                infoWindow.open(map, marker);
+            });
+            google.maps.event.addListener(marker, "mouseout", function (e) {
+              infoWindow.close(map, marker);
+          });
+        })(marker, data);
+    }
+    
+  });
+};
---
+++ google_map_sample_v2/jsjson.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+import numpy as np
+from SVY21_converter import SVY21
+
+cv = SVY21()
+
+vfunc = np.vectorize(cv.computeLatLon)
+
+df = pd.read_csv('transactions.csv')
+df = df[df['x'] != -1]
+df = df[df['y'] != -1]
+df['nettPrice'] = df['nettPrice'].fillna(0)
+(df['x'], df['y']) = vfunc(df['x'], df['y'])
+df = df.astype('object')
+df.head()
+
+counter = 1
+result = {}
+for index in df.index:
+    prop = f'property_{index}'
+    attributes = df.loc[index].to_dict()
+    result[prop] = attributes
+
+with open('transactions.json', 'w') as f:
+    json.dump(result, f)
